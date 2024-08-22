373355ea6c8e929948309c1ac7b8e05d592ccb48
vnitin08 <nvermaofficial1@gmail.com>
2024-08-22 07:37:30 +0530

    add new guess and reveal method

--- src/GuessGame.ts
+++ src/GuessGame.ts
@@ -1,4 +1,4 @@
-import { Field, MerkleMap, MerkleMapWitness, method, Poseidon, SmartContract, State, state, Struct } from 'o1js';
+import { Field, MerkleMap, MerkleMapWitness, method, Poseidon, Provable, SmartContract, State, state, Struct } from 'o1js';
 
 export class HiddenValue extends Struct({
     value: Field,
@@ -12,6 +12,8 @@ export class HiddenValue extends Struct({
 export class GuessGame extends SmartContract {
     @state(Field) hiddenNumber = State<Field>();
     @state(Field) scoreRoot = State<Field>();
+    @state(Field) guessedNumber = State<Field>();
+    @state(Field) guesser = State<Field>();
 
     init() {
         super.init();
@@ -32,33 +34,56 @@ export class GuessGame extends SmartContract {
         this.hiddenNumber.set(hiddenValue.hash());
     }
 
-    @method async guessNumber(
-        number: Field,
-        score: Field,
-        scoreWitness: MerkleMapWitness
-    ) {
-        let curHiddenNumber = this.hiddenNumber.getAndRequireEquals();
-
-        curHiddenNumber.assertEquals(
-        Poseidon.hash([number]),
-        'Other number was guessed'
-        );
-
-        // Check witnessed value
-        const [prevScoreRoot, key] = scoreWitness.computeRootAndKeyV2(score);
-
-        this.scoreRoot
-        .getAndRequireEquals()
-        .assertEquals(prevScoreRoot, 'Wrong score witness');
-
+    @method async guessNumber(number: Field) {
+        let curGuessedNumber = this.guessedNumber.getAndRequireEquals();
+    
+        curGuessedNumber.assertEquals(Field(0), "You have already guessed number");
+    
         const sender = this.sender.getAndRequireSignature();
         const senderHash = Poseidon.hash(sender.toFields());
-        key.assertEquals(senderHash, 'Witness for wrong user');
+    
+        this.guessedNumber.set(number);
+        this.guesser.set(senderHash);
+    }
 
-        const [newScoreRoot] = scoreWitness.computeRootAndKeyV2(score.add(1));
-
+    @method async revealNumber(
+        hiddenValue: HiddenValue,
+        score: Field,
+        scoreWitness: MerkleMapWitness
+      ) {
+        // Check hidden value
+        let currentHiddenNumber = this.hiddenNumber.getAndRequireEquals();
+        currentHiddenNumber.assertEquals(
+          hiddenValue.hash(),
+          'It is not hidden number'
+        );
+    
+        // Check score witness
+        const [prevScoreRoot, key] = scoreWitness.computeRootAndKeyV2(score);
+    
+        this.scoreRoot
+          .getAndRequireEquals()
+          .assertEquals(prevScoreRoot, 'Wrong score witness');
+    
+        const guesserHash = this.guesser.getAndRequireEquals();
+        key.assertEquals(guesserHash, 'Witness for wrong user');
+    
+        // Check guess
+        const guessedNumber = this.guessedNumber.getAndRequireEquals();
+        const scoreDiff = Provable.if(
+          hiddenValue.value.equals(guessedNumber),
+          Field(1),
+          Field(0)
+        );
+    
+        const [newScoreRoot] = scoreWitness.computeRootAndKeyV2(
+          score.add(scoreDiff)
+        );
+    
         this.scoreRoot.set(newScoreRoot);
         this.hiddenNumber.set(Field(0));
-    };
+        this.guessedNumber.set(Field(0));
+        this.guesser.set(Field(0));
+    }
 }
 
